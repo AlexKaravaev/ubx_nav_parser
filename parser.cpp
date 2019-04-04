d1498cf3543ae2ef708258284ace3987402b4f39
AlexKaravaev <alexkaravev@gmail.com>
2019-04-04 21:52:16 +0300

    HD_Euler message parser should be done

--- parser.cpp
+++ parser.cpp
@@ -1,5 +1,7 @@
 #include <cmath>
 #include <iostream>
+#include <vector>
+
 #define SYNC1 0xB5
 #define SYNC2 0x62 
 #define BAUDRATE B460800
@@ -9,6 +11,12 @@
 #define HPPOSLLH 0x14 // Msg class with high-precision position
 #define POSECEF 0x01// Msg class with usual euler angles
 #define POSLLH 0x02 // Msg class with usual position
+
+#define HD_EULER_OFFSET 8 
+#define HD_EULER_PAYLOAD_LEN 28
+
+#define HD_POS_OFFSET 8
+#define HD_POS_PAYLOAD_LEN 36
 
 
 class Parser{
@@ -52,7 +60,7 @@ void Parser::read_data(char* buf, int fd){
                     if(buf[cursor++] == NAV_CLASS){
                         switch(buf[cursor]){
                             case HPPOSECEF:
-                                std::cout << "MOCK";
+                                Parser::parse_HD_EULER();
                             case HPPOSLLH:
                                 std::cout << "MOCK";
                             case POSECEF:
@@ -68,14 +76,39 @@ void Parser::read_data(char* buf, int fd){
     }
 }
 
-void Parser::parse_HD_EULER(){
-    int* ecefX, ecefY, ecefZ;
-
-    for (auto i = Parser::cursor + HD_EULER_OFFSET; i < Parser::nRead; i++){
-        
-    }
+std::vector<int> Parser::parse_HD_EULER(){
     
+    // Vector = {ecefX, ecefY, ecefZ, ecefX_HP, ecefY_HP, ecefZ_hp}
+    // 8, 12, 16, 20, 21, 22 - bytes adresses
+    std::vector<int> pre_ece;
+    
+    // Final HD euler angles
+    std::vector<int> ece;
+    
+    auto i = Parser::cursor + HD_EULER_OFFSET;
+
+    //TODO: dafuck don't know how to read bytes
+    
+    for(auto i = Parser::cursor + HD_EULER_OFFSET; i < Parser::nRead; i+=4){
+    
+       pre_ece.push_back(int((unsigned char)(Parser::buf[i]) << 24 |
+                (unsigned char)(Parser::buf[i+1]) << 16 |
+                (unsigned char)(Parser::buf[i+2]) << 8 |
+                (unsigned char)(Parser::buf[i+3])));
+    }
+
+    for (auto i = 0; i < 3; i+=1){
+        ece.push_back(pre_ece[i] + (pre_ece[i+3] * 1e-2))
+    }
+
+    Parser::cursor += HD_EULER_PAULOAD_LEN;
+    return ece
 }
+
+std::vector<int> Parser::parse_HD_POS(){
+        
+}
+
 /*
 Parser::Parser(std::string PortName = "/dev/ttyACM0"){
     Parser::file_descr = open(PortName, O_RDWR);
