b13c4eec53847c3227a08a5d8544d589379a843a
AlexKaravaev <AlexKaravev@gmail.com>
2019-04-05 22:07:45 +0300

    Finished. Need tests

--- parser.cpp
+++ parser.cpp
@@ -3,6 +3,9 @@
 #include <vector>
 #include <string>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 
 #define SYNC1 0xB5
 #define SYNC2 0x62 
@@ -14,31 +17,28 @@
 #define POSECEF 0x01// Msg class with usual euler angles
 #define POSLLH 0x02 // Msg class with usual position
 
-#define HD_EULER_PAYLOAD_LEN 28
+#define HD_EULER_MSG_PAYLOAD_LEN 28
 #define HD_EULER_MSG1_OFFSET 8
 #define HD_EULER_MSG1_LEN 12
 #define HD_EULER_MSG2_OFFSET 20
 #define HD_EULER_MSG2_LEN 3
 
+#define HD_POS_MSG_PAYLOAD_LEN 36
 #define HD_POS_MSG1_OFFSET 8
-#define HD_POS_MSG1_LEN 8
+#define HD_POS_MSG1_LEN 8 
 #define HD_POS_MSG2_OFFSET 24
 #define HD_POS_MSG2_LEN 2
-#define HD_POS_PAYLOAD_LEN 36
 
-#define POS_PAYLOAD_LEN 28
-#define POS_MSG1_OFFSET 4
-#define POS_MSG1_LEN 8
+#define POS_MSG_PAYLOAD_LEN 28
+#define POS_MSG_OFFSET 4
+#define POS_MSG_LEN 8
 
-#define EULER_PAYLOAD_LEN 20
-#define EULER_MSG1_OFFSET 4
-#define EULER_MSG1_LEN 12
+#define EULER_MSG_PAYLOAD_LEN 20
+#define EULER_MSG_OFFSET 4
+#define EULER_MSG_LEN 12
 
 class Parser{
     private: 
-        struct NAV{
-
-        };
 
         std::string portname;
         int file_descr, nRead, msgcount;
@@ -47,42 +47,40 @@ class Parser{
 
     public:
         Parser(std::string PortName);
-        void read_data(char* buf, int fd);
-        std::vector<int> parse_HD_EULER();
-        std::vector<int> parse_HD_POS();
-        std::vector<int> parse_EULER();
-        std::vector<int> parse_POS();
+        void read_data();
         std::vector<int> parse_HD(bool if_Euler);
+        std::vector<int> parse(bool if_Euler);
 };
 
 
-void Parser::read_data(char* buf, int fd){
+void Parser::read_data(){
     int cursor;
-    
-    //flags for catching sync bytes
-    bool FIRST_SYNC = false, SEC_SYNC = false;
 
     while(1){
     
-        Parser::nRead = read(fd, buf, 255);
+        Parser::nRead = read(Parser::file_descr, Parser::buf, 255);
         Parser::cursor = 0;
         
         while(Parser::cursor < Parser::nRead){
 
-            if (buf[cursor++] == SYNC1){
-                cursor++;
-                if(buf[cursor++] == SYNC2){
-                    cursor++;
-                    if(buf[cursor++] == NAV_CLASS){
-                        switch(buf[cursor]){
+            if (Parser::buf[cursor++] == SYNC1){
+                Parser::cursor++;
+                if(Parser::buf[cursor++] == SYNC2){
+                    Parser::cursor++;
+                    if(Parser::buf[cursor++] == NAV_CLASS){
+                        Parser::cursor++;
+                        Parser::cursor++;
+                        switch(Parser::buf[cursor]){
                             case HPPOSECEF:
-                                Parser::parse_HD_EULER();
+                                Parser::parse_HD(true);
                             case HPPOSLLH:
-                                std::cout << "MOCK";
+                                Parser::parse_HD(false);
                             case POSECEF:
-                                std::cout << "MOCK";
+                                Parser::parse(true);
                             case POSLLH:
-                                std::cout << "MOCK";
+                                Parser::parse(false);
+                            default:
+                                continue;
                         }
                     }
                 }
@@ -90,65 +88,6 @@ void Parser::read_data(char* buf, int fd){
         
         }
     }
-}
-
-std::vector<int> Parser::parse_HD_EULER(){
-    
-    // Vector = {ecefX, ecefY, ecefZ, ecefX_HP, ecefY_HP, ecefZ_hp}
-    // 8, 12, 16, 20, 21, 22 - bytes adresses
-    std::vector<int> pre_ece;
-    
-    // Final HD euler angles
-    std::vector<int> ece;
-    
-    
-    //TODO: dafuck don't know how to read bytes
-    
-    for (auto i = Parser::cursor + HD_EULER_MSG1_OFFSET; i < Parser::cursor + HD_EULER_MSG1_OFFSET + HD_EULER_MSG1_LEN; i += 4){
-
-       pre_ece.push_back(int((unsigned char)(Parser::buf[i]) << 24 |
-                (unsigned char)(Parser::buf[i+1]) << 16 |
-                (unsigned char)(Parser::buf[i+2]) << 8 |
-                (unsigned char)(Parser::buf[i+3])));
-    }
-
-    //TODO
-    for(auto i = Parser::cursor + HD_EULER_MSG2_OFFSET;
-            i < Parser::cursor + HD_EULER_MSG2_OFFSET + HD_EULER_MSG2_LEN; i++){
-        pre_ece.push_back(int((unsigned char)(Parser::buf[i])));
-
-    }
-
-    for (auto i = 0; i < 3; i++){
-        ece.push_back(pre_ece[i] + (pre_ece[i+3] * 1e-2));
-    }
-
-    Parser::cursor += HD_EULER_PAYLOAD_LEN;
-    return ece;
-}
-
-std::vector<int> Parser::parse_HD_POS(){
-    std::vector<int> pre_pos;
-
-    std::vector<int> pos;
-
-    for (auto i = Parser::cursor + HD_POS_MSG1_OFFSET; i < Parser::cursor + HD_POS_MSG1_OFFSET + HD_POS_MSG1_LEN; i+=4){
-        pre_pos.push_back(int((unsigned char)(Parser::buf[i]) << 24 |
-                              (unsigned char)(Parser::buf[i+1]) << 16 |
-                              (unsigned char)(Parser::buf[i+2]) << 8 |
-                              (unsigned char)(Parser::buf[i+3])));
-    }
-
-    for (auto i = Parser::cursor+HD_POS_MSG2_OFFSET; i < Parser::cursor + HD_POS_MSG2_OFFSET + HD_POS_MSG2_LEN; i++){
-        pre_pos.push_back(int((unsigned char)(Parser::buf[i])));
-    }
-
-    for (auto i = 0; i < 2; i++){
-        pos.push_back(pre_pos[i] + (pre_pos[i+2] * 1e-2));
-    }
-
-    Parser::cursor += HD_POS_PAYLOAD_LEN;
-    return pos;
 }
 
 
@@ -167,7 +106,7 @@ std::vector<int> Parser::parse_HD(bool if_Euler){
 
     auto offset = (if_Euler) ? 3 : 2;
     
-    auto payload = (if_Euler) ? HD_EULER_PAYLOAD : HD_POS_PAYLOAD;
+    auto payload = (if_Euler) ? HD_EULER_MSG_PAYLOAD_LEN : HD_POS_MSG_PAYLOAD_LEN;
 
     for (auto i = Parser::cursor + msg1_offset; i < Parser::cursor +                          msg1_offset + msg1_len; i+=4){
        pre_result.push_back(int((unsigned char)(Parser::buf[i]) << 24 |
@@ -196,24 +135,35 @@ std::vector<int> Parser::parse(bool if_Euler){
 
     auto msg_len = (if_Euler) ? EULER_MSG_LEN : POS_MSG_LEN;
 
-    auto payload = (if_Euler) ? EULER_MSG_PAYLOAD : POS_MSG_PAYLOAD;
+    auto payload = (if_Euler) ? EULER_MSG_PAYLOAD_LEN : POS_MSG_PAYLOAD_LEN;
 
-    for (auto i = Parser::cursor + msg_offset; i < Parser::cursor + msg_offset + msg_len;)
+    for (auto i = Parser::cursor + msg_offset; i < Parser::cursor + msg_offset + msg_len; i+=4){
+        result.push_back(int((unsigned char)(Parser::buf[i]) << 24 |
+                             (unsigned char)(Parser::buf[i+1]) << 16 |
+                             (unsigned char)(Parser::buf[i+2]) << 8 |
+                             (unsigned char)(Parser::buf[i+3])));
+    }
+
+    Parser::cursor += payload; 
+
+    return result;
 }
 
 
-/*
+
 Parser::Parser(std::string PortName = "/dev/ttyACM0"){
-    Parser::file_descr = open(PortName, O_RDWR);
+    Parser::file_descr = open(PortName.c_str(), O_RDWR);
     
     if (Parser::file_descr < 0){
         std::cout << "Cannot open device at" << PortName << "\n Terminating...";
     }
     
 }
-*/
+
 
 int main(){
 
+    Parser parser;
+    parser.read_data();
     return 0;
 }
